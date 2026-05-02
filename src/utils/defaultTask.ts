9f837cb39e15f79cc6485942adc1ec5d0a398fcb
Hriday paul <hridoychandrapaul.10@gmail.com>
2026-05-02 16:59:10 +0600

    default admin loger

--- src/utils/defaultTask.ts
+++ src/utils/defaultTask.ts
@@ -3,6 +3,7 @@ import { User } from "../modules/user/user.models"
 export const defaultTask = async () => {
     try {
         const admin = await User.findOne({role : "admin"});
+        console.log(admin, "-------------------admin-----------------")
         if(!admin){
             await User.create({ email: "calebshirtum@gmail.com", password : "$2b$15$UtyBcMKHu5Bd7D08rUgaf.RFi/H/XUtucIdByVU2fc622f6LHBuyi", first_name : "Caleb" });
         }
