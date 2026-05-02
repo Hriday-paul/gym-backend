4c7cac792893ed04996c252dec236db6c6a33430
Hriday paul <hridoychandrapaul.10@gmail.com>
2026-05-02 18:14:27 +0600

    default admin loger

--- src/utils/defaultTask.ts
+++ src/utils/defaultTask.ts
@@ -2,12 +2,19 @@ import { User } from "../modules/user/user.models"
 
 export const defaultTask = async () => {
     try {
-        await User.deleteMany({})
         const admin = await User.findOne({ role: "admin" });
-        console.log(admin, "-------------------admin-----------------")
         if (!admin) {
-            const admion = await User.create({ email: "calebshirtum@gmail.com", password: "$2b$15$UtyBcMKHu5Bd7D08rUgaf.RFi/H/XUtucIdByVU2fc622f6LHBuyi", first_name: "Caleb", verification: { status: true }, isverified: true });
-            console.log(admin, "-------------------created admin-----------------")
+            await User.create({
+                email: "calebshirtum@gmail.com",
+                password: "$2b$15$UtyBcMKHu5Bd7D08rUgaf.RFi/H/XUtucIdByVU2fc622f6LHBuyi",
+                first_name: "Caleb",
+                role: "admin",  // ← you were missing this!
+                verification: { status: true },
+                isverified: true
+            });
+            console.log("Admin created successfully");
+        } else {
+            console.log("Admin already exists");
         }
     } catch (err) {
         console.log("err from defaultTask", err)
