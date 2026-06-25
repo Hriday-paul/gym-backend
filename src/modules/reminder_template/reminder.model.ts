ab462c17397ea2e4b5c983dcae8dfa868f224a5d
Hriday paul <hridoychandrapaul.10@gmail.com>
2026-06-25 12:43:20 +0600

    dlt templates

--- src/modules/reminder_template/reminder.model.ts
+++ src/modules/reminder_template/reminder.model.ts
@@ -6,6 +6,7 @@ const ReminderSchema = new Schema<IMatReminderTemplate>(
         name: {
             type: String,
             required: true,
+            enum: ["template"],
             default: "template"
         },
         title: {
