ab54335c1b4415e4cd9a5cba939a69a1fc719d82
Hriday paul <hridoychandrapaul.10@gmail.com>
2026-09-22 11:54:31 +0600

    rmved mat reminder all queues script

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -110,10 +110,6 @@ const loginUser = async (payload: { email: string, password: string, fcmToken?: 
         60 * 60 * 24 * 30, // 30 days
     );
 
-    await matReminderQueue.obliterate({
-        force: true,
-    });
-
     return {
         user: userDoc,
         accessToken,
